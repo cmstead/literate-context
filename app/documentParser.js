d67c9ddc88720944236226898ceefe070281532b
Chris Stead <cmstead@gmail.com>
2019-09-12 16:06:34 -0500

    [Refactor] Removed dead code

--- app/documentParser.js
+++ app/documentParser.js
@@ -13,42 +13,6 @@ function documentParser(
     const startDirectiveBlock = /^\s*\/\* lctx-start\[.*\*\/$/;
     const endDirectiveBlock = /^\s*\/\* lctx-end\[.*\*\/$/;
 
-    const attributePattern = /([^\s]+:\s+"[^"]+")|([^\s]+:\s+\[[^\]]+\])/g;
-    const stringPattern = /^"([^"]+)"$/;
-    const arrayPattern = /^\[([^\]]+)\]$/;
-
-    function parseString(attributeValue) {
-        return attributeValue.replace(stringPattern, '$1');
-    }
-
-    function parseArray(attributeValue) {
-        return attributeValue
-            .replace(arrayPattern, '$1')
-            .split(/,\s*/ig);
-    }
-
-    function parseValue(attributeValue) {
-        const isStringValue = stringPattern.test(attributeValue);
-        const parse = isStringValue ? parseString : parseArray;
-
-        return parse(attributeValue);
-    }
-
-    function getAttributes(definitionLine) {
-        return definitionLine
-            .match(attributePattern)
-            .map(attribute => {
-                const [key, value] = attribute.split(': ');
-
-                return [key, parseValue(value)];
-            })
-            .reduce(function (attributeMap, [key, value]) {
-                attributeMap[key] = value;
-
-                return attributeMap;
-            }, {});
-    }
-
     function getSubtype(definitionLine) {
         return definitionLine.replace(/^\/\*.*lctx-start\[([^\]]+)\].*\*\/$/, '$1');
     }
